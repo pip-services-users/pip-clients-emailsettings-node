42a40623a9cb512fe37c7749d0b1004a8c64a310
Sergey Seroukhov <seroukhov@gmail.com>
2017-10-27 15:04:39 -0700

    Relaxed data structures

--- src/version1/EmailSettingsV1.ts
+++ src/version1/EmailSettingsV1.ts
@@ -5,15 +5,15 @@ export class EmailSettingsV1 implements IStringIdentifiable {
     public id: string;
     public name: string;
     public email: string;
-    public language: string;
+    public language?: string;
 
     /* EmailSettings management */
-    public subscriptions: any;
-    public verified: boolean;
-    public ver_code: string;
-    public ver_expire_time: Date;
+    public subscriptions?: any;
+    public verified?: boolean;
+    public ver_code?: string;
+    public ver_expire_time?: Date;
 
     /* Custom fields */
-    public custom_hdr: any;
-    public custom_dat: any;
+    public custom_hdr?: any;
+    public custom_dat?: any;
 }
