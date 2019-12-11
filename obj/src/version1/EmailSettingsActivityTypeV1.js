d71efc57fd87065c13a2fdd36c386c69aaa30a80
Sergey Seroukhov <seroukhov@gmail.com>
2019-12-11 13:30:32 -0700

    Recompiled

--- obj/src/version1/EmailSettingsActivityTypeV1.js
+++ obj/src/version1/EmailSettingsActivityTypeV1.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 class EmailSettingsActivityTypeV1 {
 }
 exports.EmailSettingsActivityTypeV1 = EmailSettingsActivityTypeV1;
 EmailSettingsActivityTypeV1.EmailVerified = 'email verified';
-=======
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class EmailSettingsActivityTypeV1 {
-}
-exports.EmailSettingsActivityTypeV1 = EmailSettingsActivityTypeV1;
-EmailSettingsActivityTypeV1.EmailVerified = 'email verified';
->>>>>>> 6e60013bfa67c7e599865eea8b4bf06f93bfe632
 //# sourceMappingURL=EmailSettingsActivityTypeV1.js.map
